3546f2f1eb1b8c42251e3c3bd603029193fba8fb
Jason Stajich <jason@bioperl.org>
2012-12-10 13:09:22 -0800

    update width

--- results/phylo_by_clade/lens.R
+++ results/phylo_by_clade/lens.R
@@ -8,7 +8,7 @@ lnonH3K27me3_genes <- read.table("nonH3K27me3_genes_lengths.dat",header=T,sep="\
 summary(lnonH3K27me3_genes$LEN)
 summary(subset(lnonH3K27me3_genes$LEN ,lnonH3K27me3_genes$LEN < 4000))
 boxplot(subset(lH3K27me3_bordergenes$LEN,lH3K27me3_bordergenes$LEN < 4000),subset(lH3K27me3_genes$LEN,lH3K27me3_genes$LEN < 4000),subset(lnonH3K27me3_genes$LEN,lnonH3K27me3_genes$LEN < 4000),xlab="K27 Status", main="Protein size for K27 status",ylab="Protein length",range=2,names=c("Border","K27","Not K27"),col=rainbow(11, start=0, end=1));
-boxplot(subset(lH3K27me3_genes$LEN,lH3K27me3_genes$LEN < 4000),subset(lnonH3K27me3_genes$LEN,lnonH3K27me3_genes$LEN < 4000),xlab="K27 Status", main="Protein size for K27 status",ylab="Protein length",range=2,names=c("K27","Not K27"),col=c("Green","Blue"));
+boxplot(subset(lH3K27me3_genes$LEN,lH3K27me3_genes$LEN < 4000),subset(lnonH3K27me3_genes$LEN,lnonH3K27me3_genes$LEN < 4000),xlab="K27 Status", main="Protein size for K27 status",ylab="Protein length",range=2,names=c("K27","Not K27"),col=c("Green","Blue"),width=c(0.5,0.5));
 
 t.test(subset(lH3K27me3_genes$LEN,lH3K27me3_genes$LEN < 4000),subset(lnonH3K27me3_genes$LEN,lnonH3K27me3_genes$LEN < 4000));
 t.test(lH3K27me3_genes$LEN,lnonH3K27me3_genes$LEN);
